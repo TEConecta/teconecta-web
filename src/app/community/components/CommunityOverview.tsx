eda23a39aa09d14d9fd37f632c6a2a630f45d671
Andre <lluviadefuego1@hotmail.com>
2024-10-31 00:22:05 -0600

    Agregada la función de busqueda y no me acuerdo si ya habia subido el componente de las comunidades XD

---
+++ src/app/community/components/CommunityOverview.tsx
@@ -0,0 +1,49 @@
+'use client';
+import React, { useState } from 'react';
+
+interface CommunityCardProps {
+    name: string;
+    image: string;
+    description: string;
+    members: number;
+}
+
+const ComunityOverview: React.FC<CommunityCardProps> = ({
+    name,
+    image,
+    description,
+    members,
+}) => {
+    const [isMember, setIsMember] = useState(false);
+
+    const handleJoinClick = () => {
+        setIsMember(!isMember);
+    };
+
+    return (
+        <div className='my-6'>
+            <div className='rounded-lg'>
+                <div className='px- flex items-center justify-between'>
+                    <div className='flex items-center'>
+                        <img src={image} alt="icon" className='w-32 h-auto rounded-full' />
+                        <section className='ml-2'>
+                            <p className='font-bold text-2xl'>/{name}</p>
+                            <p className='text-gray-500 text-sm font-semibold'>{members} miembros.</p>
+                        </section>
+                    </div>
+                    <button
+                        className={`px-4 py-1 cursor-pointer rounded-md ${isMember ? 'bg-white text-black border border-black' : 'bg-blue-950 text-white'}`}
+                        onClick={handleJoinClick}
+                    >
+                        {isMember ? 'Miembro' : 'Unirme'}
+                    </button>
+                </div>
+                <div className='px-2 flex items-center'>
+                    <p className='text-gray-500 font-semibold text-justify text-sm'>{description}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ComunityOverview;
--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,31 +1,41 @@
 import React from 'react'
 import CommunityCard from './components/CommunityCard'
+import ComunityOverview from './components/CommunityOverview'
+import { HeaderNav, NewPublication, PostComponent } from '../posts/components'
 
 const page = () => {
-  return (
-    <div>
-      <CommunityCard
-        name='Mecatronicos'
-        image='/imgs/logo_guinda.png'
-        description='Aqui nadie sabe como arreglar nada pero los memes sobran. Mecatronica en modo desmadre.'
-        members={250} />
-        <CommunityCard
-        name='Mecatronicos'
-        image='/imgs/logo_guinda.png'
-        description='Aqui nadie sabe como arreglar nada pero los memes sobran. Mecatronica en modo desmadre.'
-        members={250} />
-        <CommunityCard
-        name='Mecatronicos'
-        image='/imgs/logo_guinda.png'
-        description='Aqui nadie sabe como arreglar nada pero los memes sobran. Mecatronica en modo desmadre.'
-        members={250} />
-        <CommunityCard
-        name='Mecatronicos'
-        image='/imgs/logo_guinda.png'
-        description='Aqui nadie sabe como arreglar nada pero los memes sobran. Mecatronica en modo desmadre.'
-        members={250} />
-    </div>
-  )
+    return (
+        <div>
+            <HeaderNav />
+            <div className='px-2'>
+                <ComunityOverview
+                    name='Mecatronicos'
+                    image='/imgs/perfil.jpeg'
+                    description='Aquí nadie sabe como arreglar nada pero los memes sobran. Mecatrónica en modo desmadre.'
+                    members={250}
+                />
+            </div>
+            <div className="h-1 bg-red-900 shadow-md my-2"></div>
+            <div>
+                <NewPublication />
+                <div className="  scrollable-container relative h-screen  ">
+
+                    {[...Array(5)].map((_, index) => (
+                        <PostComponent
+                            key={index}
+                            username="OliverElGuapo666"
+                            alias="Mecatronicos"
+                            timeAgo="3h"
+                            question="¿Cuál es el peor maestro que les ha tocado?"
+                            content="Hugo nos daba demasiados proyectos difíciles y nunca explicaba bien los temas en clase."
+                            likes={300}
+                            imageUrl={"/imgs/perfil.jpeg"}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default page
