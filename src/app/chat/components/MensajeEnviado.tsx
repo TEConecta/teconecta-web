a61ad25da9074cecdeb088b06dae3f50c3867fc9
eddy3o <edgardo.gonzalez.23@hotmail.com>
2024-11-05 11:36:30 -0600

    correccion de tipo

--- src/app/chat/components/MensajeEnviado.tsx
+++ src/app/chat/components/MensajeEnviado.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const MensajeEnviado = ({ mensaje }) => {
+const MensajeEnviado = ({ mensaje }: { mensaje: string }) => {
   return (
     <div className="absolute bottom-16 right-0 p-4 z-10">
       <div className="rounded-full bg-red-900 text-white px-4 py-2 max-w-xs">
