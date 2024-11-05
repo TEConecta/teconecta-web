59e8a6294ebb2dfbed103d74b001ec58cf3f97c4
eddy3o <edgardo.gonzalez.23@hotmail.com>
2024-11-05 11:37:53 -0600

    format

--- src/app/community/components/CommunityCard.tsx
+++ src/app/community/components/CommunityCard.tsx
@@ -1,49 +1,57 @@
-'use client';
-import React, { useState } from 'react';
+"use client";
+import React, { useState } from "react";
 
 interface CommunityCardProps {
-    name: string;
-    image: string;
-    description: string;
-    members: number;
+  name: string;
+  image: string;
+  description: string;
+  members: number;
 }
 
 const CommunityCard: React.FC<CommunityCardProps> = ({
-    name,
-    image,
-    description,
-    members,
+  name,
+  image,
+  description,
+  members,
 }) => {
-    const [isMember, setIsMember] = useState(false);
+  const [isMember, setIsMember] = useState(false);
 
-    const handleJoinClick = () => {
-        setIsMember(!isMember);
-    };
+  const handleJoinClick = () => {
+    setIsMember(!isMember);
+  };
 
-    return (
-        <div className='my-6'>
-            <div className='border border-gray-500 rounded-lg p-4'>
-                <div className='px-2 flex items-center justify-between'>
-                    <div className='flex items-center'>
-                        <img src={image} alt="icon" className='w-16 h-auto' />
-                        <section className='ml-2'>
-                            <p className='font-bold'>/{name}</p>
-                            <p className='text-gray-500 text-sm font-semibold'>{members} miembros.</p>
-                        </section>
-                    </div>
-                    <button
-                        className={`px-6 py-1 cursor-pointer mr-6 rounded-md ${isMember ? 'bg-white text-black border border-black' : 'bg-blue-950 text-white'}`}
-                        onClick={handleJoinClick}
-                    >
-                        {isMember ? 'Miembro' : 'Unirme'}
-                    </button>
-                </div>
-                <div className='px-2 flex items-center mt-2'>
-                    <p className='text-gray-500 font-semibold text-justify text-sm'>{description}</p>
-                </div>
-            </div>
+  return (
+    <div className="my-6">
+      <div className="border border-gray-500 rounded-lg p-4">
+        <div className="px-2 flex items-center justify-between">
+          <div className="flex items-center">
+            <img src={image} alt="icon" className="w-16 h-auto" />
+            <section className="ml-2">
+              <p className="font-bold">/{name}</p>
+              <p className="text-gray-500 text-sm font-semibold">
+                {members} miembros.
+              </p>
+            </section>
+          </div>
+          <button
+            className={`px-6 py-1 cursor-pointer mr-6 rounded-md ${
+              isMember
+                ? "bg-white text-black border border-black"
+                : "bg-blue-950 text-white"
+            }`}
+            onClick={handleJoinClick}
+          >
+            {isMember ? "Miembro" : "Unirme"}
+          </button>
         </div>
-    );
+        <div className="px-2 flex items-center mt-2">
+          <p className="text-gray-500 font-semibold text-justify text-sm">
+            {description}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CommunityCard;
--- src/app/community/searchCommunity/page.tsx
+++ src/app/community/searchCommunity/page.tsx
@@ -1,59 +1,75 @@
-'use client'
-import React, { useState } from 'react'
-import CommunityCard from '../components/CommunityCard'
+"use client";
+import React, { useState } from "react";
+import CommunityCard from "../components/CommunityCard";
 
-const page = () => {
-
-  const [search, setSearch] = useState('')
+const Page = () => {
+  const [search, setSearch] = useState("");
 
   const communities = [
     {
-      name: 'Mecatronicos',
-      image: '/imgs/logo_guinda.png',
-      description: 'Aquí nadie sabe como arreglar nada pero los memes sobran. Mecatrónica en modo desmadre.',
+      name: "Mecatronicos",
+      image: "/imgs/logo_guinda.png",
+      description:
+        "Aquí nadie sabe como arreglar nada pero los memes sobran. Mecatrónica en modo desmadre.",
       members: 250,
     },
     {
-      name: 'Pelos y cebolla',
-      image: '',
-      description: 'Aqui vamos a hablar de puros pelos huevo cebolla. O sea es una comunidad de cocina.',
+      name: "Pelos y cebolla",
+      image: "",
+      description:
+        "Aqui vamos a hablar de puros pelos huevo cebolla. O sea es una comunidad de cocina.",
       members: 666,
     },
     {
-      name: 'Peditas tec',
-      image: '/imgs/logo_guinda.png',
-      description: 'Comunidad hecha exclusivamente para programarse unas peditas bien maquiavelicas bien aca bien asi',
+      name: "Peditas tec",
+      image: "/imgs/logo_guinda.png",
+      description:
+        "Comunidad hecha exclusivamente para programarse unas peditas bien maquiavelicas bien aca bien asi",
       members: 250,
     },
     {
-      name: 'Mecatronicos',
-      image: '/imgs/logo_guinda.png',
-      description: 'Aquí nadie sabe como arreglar nada pero los memes sobran. Mecatrónica en modo desmadre.',
+      name: "Mecatronicos",
+      image: "/imgs/logo_guinda.png",
+      description:
+        "Aquí nadie sabe como arreglar nada pero los memes sobran. Mecatrónica en modo desmadre.",
       members: 250,
     },
     {
-      name: 'Pelos y cebolla',
-      image: '',
-      description: 'Aqui vamos a hablar de puros pelos huevo cebolla. O sea es una comunidad de cocina.',
+      name: "Pelos y cebolla",
+      image: "",
+      description:
+        "Aqui vamos a hablar de puros pelos huevo cebolla. O sea es una comunidad de cocina.",
       members: 666,
     },
     {
-      name: 'Peditas tec',
-      image: '/imgs/logo_guinda.png',
-      description: 'Comunidad hecha exclusivamente para programarse unas peditas bien maquiavelicas bien aca bien asi',
+      name: "Peditas tec",
+      image: "/imgs/logo_guinda.png",
+      description:
+        "Comunidad hecha exclusivamente para programarse unas peditas bien maquiavelicas bien aca bien asi",
       members: 250,
     },
-  ]
+  ];
 
-  const filteredCommunities = communities.filter(community =>
+  const filteredCommunities = communities.filter((community) =>
     community.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
   );
 
   return (
-    <div className='p-6'>
-      <div className='flex rounded-full px-4 py-2 bg-gray-200'>
-        <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-          <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 3.5a7 7 0 100 14 7 7 0 000-14zM21 21l-4.35-4.35" />
+    <div className="p-6">
+      <div className="flex rounded-full px-4 py-2 bg-gray-200">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="mr-2 h-6 w-6 text-gray-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M10.5 3.5a7 7 0 100 14 7 7 0 000-14zM21 21l-4.35-4.35"
+          />
         </svg>
         <input
           type="text"
@@ -73,7 +89,7 @@ const page = () => {
         />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
